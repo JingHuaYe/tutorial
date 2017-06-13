a64fe485e217181a7e86b7c5772c0790982c5494
JingHua Ye <jinghua.ye@cit.ie>
2017-06-13 14:43:56 +0100

    full solution that fixes #3

--- src/power.py
+++ src/power.py
@@ -2,23 +2,24 @@
  This is a script that calculates one number to the power of another
 """
 import math
+import sys
 
 def to_the_power(x, y):
-""" 
+ """ 
  y = 2
  result = x
  for i in range(0, y):
   result *= x
  return result
-"""
+ """
  return x ** y
 
 def square_root(x):
  return math.sqrt(x)
 
 def main():
- x = 9
- y = 8
+ x = int(sys.argv[1])
+ y = int(sys.argv[2])
  print "{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y))
  print "The square root of {0} is {1}".format(x, square_root(x))
  print "The End"
