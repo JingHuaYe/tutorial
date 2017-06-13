e425c6ca30681ac2e526c7486c7dc188e5e3ec14
JingHua Ye <jinghua.ye@cit.ie>
2017-06-13 14:03:56 +0100

    ADD SQUARE ROOT SUCCESSFULLY

--- src/power.py
+++ src/power.py
@@ -1,6 +1,8 @@
 """
  This is a script that calculates one number to the power of another
 """
+import math
+
 def to_the_power(x, y):
 """ 
  y = 2
@@ -11,10 +13,14 @@ def to_the_power(x, y):
 """
  return x ** y
 
+def square_root(x):
+ return math.sqrt(x)
+
 def main():
  x = 9
  y = 8
  print "{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y))
+ print "The square root of {0} is {1}".format(x, square_root(x))
  print "The End"
 
 main()
