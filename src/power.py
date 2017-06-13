43ddc95471721ca991e7e54199aa2153eca08edc
JingHua Ye <jinghua.ye@cit.ie>
2017-06-13 13:42:36 +0100

    made speculative changes

--- src/power.py
+++ src/power.py
@@ -2,15 +2,18 @@
  This is a script that calculates one number to the power of another
 """
 def to_the_power(x, y):
+""" 
  y = 2
  result = x
  for i in range(0, y):
   result *= x
  return result
+"""
+ return x ** y
 
 def main():
  x = 9
- y = 7 #different
+ y = 8
  print "{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y))
  print "The End"
 
